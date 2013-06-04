685cfc4df893d5fed64a4bf8e2b1221cfbf6a9fa
Sreevatsa <sreevatsa.ks@gmail.com>
2013-06-04 01:19:16 -0700

    Refactored some code<sreeks>

--- product/TestClient/src/edu/uci/testclient/Main.java
+++ product/TestClient/src/edu/uci/testclient/Main.java
@@ -25,9 +25,9 @@ public class Main {
 			System.out.println("run type is either \"TestInvoker\" or \"TestRunner\"");
 			return;
 		}
-		if(args[0] == "Testinvoker")
+		if(args[0] == "TestInvoker")
 			try {
-				processTestInvoker(args);
+				new TestInvoker().process(args);
 			} catch (IOException e) {
 				System.out.println("Error: " + e.getMessage());
 			}
@@ -38,41 +38,4 @@ public class Main {
 	private static void processTestRunner(String[] args) {
 		throw new NotImplementedException();
 	}
-
-	private static void processTestInvoker(String[] args) throws IOException {
-	    JarFile jarFile = new JarFile(args[0]);
-	    Enumeration<JarEntry> e = jarFile.entries();
-
-	    URL[] urls = new URL[args.length];
-	    int argIndex = 0;
-	    for (String string : args) {
-			urls[argIndex] = new URL("jar:file:" + args[argIndex++] +"!/");
-		}
-	    URLClassLoader cl = URLClassLoader.newInstance(urls);
-
-	    while (e.hasMoreElements()) {
-	       JarEntry je = (JarEntry) e.nextElement();
-           if(je.isDirectory() || !je.getName().endsWith(".class") || je.getName().contains("$")){
-               continue;
-           }	
-	
-	       String className = je.getName().substring(0,je.getName().length()-6);
-		   className = className.replace('/', '.');
-		   try {
-			   Class c = cl.loadClass(className);
-			   System.out.println(c.getName());
-			   TestClass testClass = new TestClass(c);
-			   List<FrameworkMethod> testList = testClass.getAnnotatedMethods(org.junit.Test.class);
-		   } 
-		   catch (ClassNotFoundException e1) {
-			   e1.printStackTrace();
-		   } 
-		   catch (Exception e1) {
-			   e1.printStackTrace();
-		   }
-		
-	}
-	   jarFile.close();
-	}
-
 }
---
+++ product/TestClient/src/edu/uci/testclient/TestInvoker.java
@@ -0,0 +1,58 @@
+package edu.uci.testclient;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.TestClass;
+
+import edu.uci.util.MsgFormat;
+
+public class TestInvoker {
+
+	public void process(String[] args) throws IOException {
+		 JarFile jarFile = new JarFile(args[1]);
+		    Enumeration<JarEntry> e = jarFile.entries();
+
+		    URL[] urls = new URL[args.length];
+		    int argIndex = 0;
+		    for (String string : args) {
+				urls[argIndex] = new URL("jar:file:" + args[argIndex++] +"!/");
+			}
+		    URLClassLoader cl = URLClassLoader.newInstance(urls);
+
+		    while (e.hasMoreElements()) {
+		       JarEntry je = (JarEntry) e.nextElement();
+	           if(je.isDirectory() || !je.getName().endsWith(".class") || je.getName().contains("$")){
+	               continue;
+	           }	
+		
+		       String className = je.getName().substring(0,je.getName().length()-6);
+			   className = className.replace('/', '.');
+			   try {
+				   Class c = cl.loadClass(className);
+				   System.out.println(c.getName());
+				   TestClass testClass = new TestClass(c);
+				   List<FrameworkMethod> testList = testClass.getAnnotatedMethods(org.junit.Test.class);
+				   for (FrameworkMethod frameworkMethod : testList) {
+					   MsgFormat msg = new MsgFormat();
+					   msg.setTestName(frameworkMethod.getName());
+				   }
+			   } 
+			   catch (ClassNotFoundException e1) {
+				   e1.printStackTrace();
+			   } 
+			   catch (Exception e1) {
+				   e1.printStackTrace();
+			   }
+			
+		}
+		jarFile.close();
+	}
+
+}
